49e3fb66952e829dd93ddd12534685d357eac764
ngoctin02806 <ngoctin02806@gmail.com>
2021-05-19 14:27:00 +0700

    Login

---
+++ Test Suites/Run all test cases.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run all test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c69a560c-1219-4c6e-9b90-0c703efd4292</testSuiteGuid>
+   <testCaseLink>
+      <guid>b531ec90-efb9-4aa9-a047-6985ce991543</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c64d549f-d72d-4fa8-8d59-125e84161ea6</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginDataTest</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>c64d549f-d72d-4fa8-8d59-125e84161ea6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>a6fce1c1-fe9d-41df-9fd6-696f999baee8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>c64d549f-d72d-4fa8-8d59-125e84161ea6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>5e047d98-47ff-4374-9c61-5bb1560cbaa2</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
